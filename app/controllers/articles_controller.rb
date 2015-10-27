b462996fe65c33952e28af7980fbc93b2919280d
Thanh Nguyen <thanh.trade@gmail.com>
2015-10-27 21:39:10 +0700

    add index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def create
     @article = Article.new(article_params)
-
     @article.save
+
     redirect_to @article
   end
 
